35e0cec9f9032f43e3127122b91fb4a3b3c44028
AZamani <andre.zamani@yahoo.com>
2022-03-22 22:12:27 +0100

    Created sollution/project with initial functions

---
+++ 1337_Assignment/1337_Assignment/DownloadUtility.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+
+namespace _1337_Assignment
+{
+    public class DownloadUtility
+    {
+        public async void Run(string address, string localPath)
+        {
+
+            Task<string> requestPageTask = RequestPage(address);
+            Console.WriteLine("Reading page...");
+            requestPageTask.Wait();
+            
+            var urls = await GetUrls(requestPageTask.Result);
+
+        }
+        private async Task ConsoleLoading()
+        {
+            Console.Write('.');
+            Thread.Sleep(500);
+
+        }
+
+        private static async Task DownloadFileAsync(string uri, string localPath)
+        {
+            try
+            {
+                using (var client = new WebClient())
+                {
+                    client.DownloadFileAsync(new Uri(uri), localPath);
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        private async Task<List<string>> GetUrls(string content)
+        {
+            var urls = new List<string>();
+            const string pattern = @"a href=""(?<link>.+?)""";
+
+            Regex regex = new Regex(pattern, RegexOptions.IgnoreCase);
+
+            MatchCollection MC = regex.Matches(content);
+            foreach (Match match in MC)
+            {
+                urls.Add(match.Groups["link"].ToString());
+            }
+            return urls;
+        }
+        private async Task<string> RequestPage(string uri)
+        {
+            using (var client = new HttpClient())
+            {
+                return await client.GetStringAsync(uri);
+            }
+        }
+
+    }
+    
+}
---
+++ 1337_Assignment/1337_Assignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _1337_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            string url, localPath; 
+            Console.WriteLine("Enter a website");
+            url = Console.ReadLine();
+            Console.WriteLine("Enter localPath to download to");
+            localPath = Console.ReadLine(); 
+            */
+
+
+            
+            var util = new DownloadUtility();
+            util.Run("https://tretton37.com/", @"C:\Users\andzam\Test"); 
+
+        }
+
+
+    }
+}
